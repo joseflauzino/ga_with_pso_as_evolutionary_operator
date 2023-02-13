aef7ab898a59c70d40916ece8dee5f8433595937
abviescinski <amanda.b.vieski@gmail.com>
2023-02-13 11:27:49 -0300

    add avg of the execution by distance generation

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def main(function_name, function, bounds):
     results_mean = []
     particle_number = []
     results_best_fitness = []
+    distance_exec = []
 
     print('Optimizing the', function_name, 'function...')
     # varying the number of chromosomes
     for n in range(30, 101, 10):
+        best_fitness_t = np.inf
         ga = GA(function, bounds, pop_size=n, mt_prob=0.2, pso_mutation=True, with_inertia=True)
         partial_results = []
-        best_fitness_t = np.inf
         # executing the algorithm 30 times
         for i in range(30):
             execution, best_fitness, chromosome_list = ga.run()
@@ -26,15 +27,37 @@ def main(function_name, function, bounds):
             best_fitness.sort()
             if best_fitness_t > best_fitness[0]:
                 best_fitness_t = best_fitness[0]
-        if n == 50:
-            result_distance = calculate_population_distance(chromosome_list)
+            if n == 50:
+                distance_exec.append(calculate_population_distance(chromosome_list))
 
         particle_number.append(n)
         results.append(partial_results)
         results_mean.append(np.mean(partial_results))
         results_best_fitness.append(best_fitness_t)
 
+
     # ploting
+    #TODO: calcular as médias por execução aqui
+    result_distance = []
+    for gen in range(100):
+        aux_result_distance = []
+        for list_avg in distance_exec:
+            aux_result_distance.append(list_avg[gen])
+        #calcula a media da geração aqui
+        result_distance.append(mean(aux_result_distance))
+
+    print('Plotting Average Distance over Generations...')
+    plt.figure(figsize=(10, 7))
+    plt.plot(result_distance, 'ro')
+    plt.title(function_name)
+    plt.ylabel("Average Distance")
+    plt.xlabel("Generation")
+    plt.yticks(np.arange(0, 110, step=10))
+    plt.savefig(
+        "imgs/ga_average_distance_over_generations(" + function_name + ").png")
+    print('')
+
+    
     print('Plotting Fitness vs Population Size...')
     plt.figure(figsize=(10, 7))
     plt.plot(results_mean, 'ro')
@@ -48,17 +71,7 @@ def main(function_name, function, bounds):
     plt.savefig("imgs/ga_fitness_vs_population_size(" + function_name + ").png")
 
 
-    print('Plotting Average Distance over Generations...')
-    plt.figure(figsize=(10, 7))
-    plt.plot(result_distance, 'ro')
-    plt.title(function_name)
-    plt.ylabel("Average Distance")
-    plt.xlabel("Generation")
-    plt.yticks(np.arange(0, 110, step=10))
-    plt.savefig(
-        "imgs/ga_average_distance_over_generations(" + function_name + ").png")
 
-    print('')
 
 
 if __name__ == "__main__":
