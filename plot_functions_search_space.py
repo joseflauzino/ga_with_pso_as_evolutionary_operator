681267ae07d9ca3e9396d6f47031518cfd65cc8e
joseflauzino <jose.wilson.vf@gmail.com>
2023-02-16 10:53:45 -0300

    Save changes em plot

--- plot_functions_search_space.py
+++ plot_functions_search_space.py
@@ -5,9 +5,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from functions import *
 from util import *
 
-functions = ['sphere', 'rastringin', 'ackley', 'eggholder', 'drop_wave']
+functions = ['sphere', 'rastrigin', 'ackley', 'eggholder', 'drop_wave']
 for f in functions:
-    f_name, f_, f_bounds = get_function_and_bounds(f)
+    f_name, f_, f_bounds, f_global_minimum = get_function_and_bounds(f)
 
     print('Ploting '+f_name+'...')
 
@@ -21,6 +21,9 @@ for f in functions:
     ax = fig.add_subplot(projection='3d') 
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
     cmap=cm.nipy_spectral, linewidth=0.08,
-    antialiased=True)    
+    antialiased=True)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('f(x1, x2)')
     plt.savefig('imgs/search_space/' + f_name + '.png')
 print('Done!')
